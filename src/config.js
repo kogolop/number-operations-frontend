e8b6ab8f878fec8234aca7c5fec1d7971a2b4ad5
kogolop <kogolopaul@gmail.com>
2024-06-23 11:28:17 -0400

    updated config.js script

---
+++ src/config.js
@@ -0,0 +1,14 @@
+// src/config.js
+
+const API_CONFIG = {
+    statsApi: 'http://127.0.0.1:5000',
+    operationsApi: 'http://127.0.0.1:5002',
+    searchApi: 'http://127.0.0.1:5003',
+    conversionApi: 'http://127.0.0.1:5004',
+    sequenceApi: 'http://127.0.0.1:5005'
+};
+
+// If you're using ES6 modules, you might want to export it like this:
+// export default API_CONFIG;
+
+// For now, since we're using <script> tags, we'll keep it as a global variable
